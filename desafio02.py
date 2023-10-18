bf5e0c58512ac7440e7130d29c62e57ecf4b7930
devneyva <dev.neyva@gmail.com>
2023-10-18 16:07:36 -0300

    commit desafio.02

---
+++ desafio02.py
@@ -0,0 +1,9 @@
+(idade)= input("digite sua idade:")
+(idade_parente) = input("Digite a idade de um parente seu:")
+
+nome = input("Digite seu nome:")
+
+resultado = int(idade) * int(idade_parente) 
+print(resultado)
+print(nome)
+
---
+++ input_de_dados.py
@@ -0,0 +1,5 @@
+#para criar uma interação com o usuário utilizamos a função input.
+#todo input deve ter seu valor associado a uma variável
+
+nome = input("digite seu nome aqui:")
+print(nome)
