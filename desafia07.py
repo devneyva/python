e4da7a7a2be95a1d620c7790ae8008dc54dc08e0
devneyva <dev.neyva@gmail.com>
2023-10-19 10:52:54 -0300

    desafio7 não concluido

--- desafia07.py
+++ desafia07.py
@@ -2,4 +2,8 @@ numero1= input("Digite o primeiro número:")
 numero2= input("Digite o segundo número:")
 numero3= input("Digite o terceiro número:")
 
+produto_do_dobro_numero1 = int(numero1) * 2
+produto_metade_numero2 = int(numero2) / 2
+numero3 = int(numero3)
+print(produto_do_dobro_numero1,produto_metade_numero2 + numero3)
 
