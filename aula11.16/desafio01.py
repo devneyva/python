a1770b3ceaa62fe1f1d745c260c56df79dbc54f6
devneyva <dev.neyva@gmail.com>
2023-11-18 23:09:42 -0300

    Salvando

--- aula11.16/desafio01.py
+++ aula11.16/desafio01.py
@@ -1,3 +1,7 @@
 lista= [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-print(lista[2::2])
-print(lista.remove[4::4])
+
+
+del lista[4::4]
+lista.append("Oi")
+lista.remove("Oi")
+print(lista)
