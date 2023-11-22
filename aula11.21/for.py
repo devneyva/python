d1e0c78624c2b1d6fddd3d546ede16556ef809c5
devneyva <dev.neyva@gmail.com>
2023-11-22 14:26:01 -0300

    aula 21 commitada

---
+++ aula11.21/for.py
@@ -0,0 +1,14 @@
+#for trabalha com interáveis!!!
+#tem que possuir uma variável de controle
+# possui dois me´todos iter() next()
+
+#código que imprime os dias da semana
+
+i = 1001
+divisao = i / 100
+
+for i in range(i):
+    if i > 0:
+        print(i)
+        if % divisao == 0:
+            print(i) 
